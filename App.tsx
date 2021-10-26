21d494d6fc42af38e21716620c7629a4a773f72a
ZekeHart <heirloch@gmail.com>
2021-10-26 16:37:37 -0400

    initial commit with basic structure

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+        <TouchableOpacity >
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>Grocery List</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity >
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>To Dos</Text>
+          </View>
+        </TouchableOpacity>
+      
     </View>
   );
 }
@@ -14,8 +29,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
 });
---
+++ README.md
@@ -0,0 +1,24 @@
+# Plan:
+
+Typescript React Native prodcutivity app that allows tracking of goals, tasks, and some shopping lists.
+Uses Expo, React Navigation
+
+## To Dos Outline:
+Today
+This Week
+Long Term
+
+Recurring (times per day/week)
+One time (added to day, week, long term)
+
+## Grocery List Outline:
+Items 
+
+view old items, re-add to current list
+
+
+
+# Possibly:
+Add values to help prioritize to dos. Rate on 2 scales, ease to complete and value when completed.
+Connect to remote data store. Firebase?
+Ability to add rewards/incentives to goals?
